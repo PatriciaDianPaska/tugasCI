97cb35191a8e4f27e6bfd4666fee4c60e6af80be
PatriciaDianPaska <patricia_dian_26rpl@student.smktelkom-mlg.sch.id>
2019-01-22 13:28:17 +0700

    first commit

---
+++ application/views/event.php
@@ -0,0 +1,53 @@
+<!-- <section class="wrapper site-min-height"> -->
+<div style="margin-left:2%;margin-right:2%; margin-bottom:-3%;">
+          	<h3><i class="fa fa-angle-right"></i> Event</h3>
+          	<hr>
+				<div class="row mt">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">					
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/10.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/10.jpg" style="width:500px; height:480px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/11.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/11.jpg" style="width:500px; height:480px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/12.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/12.jpg" style="width:500px; height:480px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+				</div><!-- /row -->
+
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+				</div><!-- /row -->
+</div>
+		<!-- </section><!/wrapper -->
---
+++ application/views/gallery.php
@@ -0,0 +1,128 @@
+<!-- <section class="wrapper site-min-height"> -->
+<div style="margin-left:2%;margin-right:2%; margin-bottom:-3%;">
+          	<h3><i class="fa fa-angle-right"></i> Gallery</h3>
+          	<hr>
+				<div class="row mt">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">					
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/1.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/1.jpg" style="width:500px; height:237px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/3.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/3.jpg" style="width:500px; height:237px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/2.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/2.jpg" style="width:500px; height:237px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+				</div><!-- /row -->
+
+				<div class="row mt">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/4.jpg" style="width:500px; height:243px;"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/4.jpg" style="width:500px; height:243px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/5.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/5.jpg" style="width:500px; height:243px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/6.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/6.jpg" style="width:500px; height:243px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+				</div><!-- /row -->
+
+				<div class="row mt mb">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/7.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/7.jpg" style="width:500px; height:243px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/8.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/8.jpg" style="width:500px; height:243px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+					
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+						<div class="project-wrapper">
+		                    <div class="project">
+		                        <div class="photo-wrapper">
+		                            <div class="photo">
+		                            	<a class="fancybox" href="<?= base_url()?>assets/img/portfolio/9.jpg"><img class="img-responsive" src="<?= base_url()?>assets/img/portfolio/9.jpg" style="width:500px; height:243px;" alt=""></a>
+		                            </div>
+		                            <div class="overlay"></div>
+		                        </div>
+		                    </div>
+		                </div>
+					</div><!-- col-lg-4 -->
+				</div><!-- /row -->
+</div>
+		<!-- </section><!/wrapper -->
